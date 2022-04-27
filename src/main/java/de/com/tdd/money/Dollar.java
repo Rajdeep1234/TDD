cf66ac8357178fe9dade9991b701883c23767be3
Rajdeep Sahoo <rajdeep.sahoo@thoughtworks.com>
2022-04-27 13:25:56 +0530

    adding third test case

--- src/main/java/de/com/tdd/money/Dollar.java
+++ src/main/java/de/com/tdd/money/Dollar.java
@@ -9,10 +9,12 @@ public class Dollar {
         this.amount = amount;
     }
 
+    // Returning a new object as we cann't use the same instance
     public Dollar times(int rate){
         return new Dollar(amount*rate);
     }
 
+    //As Dollar is a value object we have to check whether its the same value and object or not
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
