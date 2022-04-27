43544ef2eec6f877732ef04d0eaf056a55f5a748
Rajdeep Sahoo <rajdeep.sahoo@thoughtworks.com>
2022-04-27 12:16:13 +0530

    commit changes

--- src/main/java/de/com/tdd/money/Expression.java
+++
@@ -1,7 +0,0 @@
-package de.com.tdd.money;
-
-public interface Expression {
-    Money reduce(Bank bank, String to);
-    Expression plus(Expression addend);
-    Expression times(int multiplier);
-}
